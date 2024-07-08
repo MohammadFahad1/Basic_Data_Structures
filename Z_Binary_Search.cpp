9d838ac74560b27062603b1a88e95a5b7af81d3b
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-07-08 19:01:35 +0600

    Z. Binary Search Solved Using Binary Search Algorithm

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -3,41 +3,47 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++)
+    long long int n, q;
+    cin >> n >> q;
+    long long int a[n];
+    for (long long int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    int x;
-    cin >> x;
 
-    int l = 0;
-    int r = n - 1;
-    bool flag = false;
-    while (l <= r)
+    sort(a, a + n);
+
+    while (q--)
     {
-        int mid_index = (l + r) / 2;
-        if (a[mid_index] == x)
-        {
-            flag = true;
-            break;
-        }
-        else if (x > a[mid_index])
-        {
-            l = mid_index + 1;
-        }
-        else
-        {
-            r = mid_index - 1;
-        }
-    }
+        int x;
+        cin >> x;
+        bool flag = false;
+        int l = 0, r = n - 1;
 
-    if (flag)
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
+        // Binary Search
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if (a[mid] == x)
+            {
+                flag = true;
+                break;
+            }
+            else if (x >= a[mid])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        if (flag)
+            cout << "found" << endl;
+        else
+            cout << "not found" << endl;
+    }
 
     return 0;
 }

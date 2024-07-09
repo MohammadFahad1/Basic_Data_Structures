34b90f5442b37cccd86025c3a743c5114f8eb43b
Md. Fahad - ABA Office PC <fahad4bangladesh@gmail.com>
2024-07-09 20:39:31 +0600

    Trying to create a dynamic node

---
+++ dynamic_node.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+
+    return 0;
+}

0bf48a0ff421cc25619f28505b18ef5843219657
Md. Fahad - ABA Office PC <fahad4bangladesh@gmail.com>
2024-07-10 16:45:50 +0600

    Creating a Dynamic Node

--- dynamic_node.cpp
+++ dynamic_node.cpp
@@ -9,13 +9,24 @@ public:
     Node(int val)
     {
         this->val = val;
+        this->next = NULL;
     }
 };
 
 int main()
 {
+    /* Static Node */
+    // Node head(10);
+
+    /* Dynamic Node */
     Node *head = new Node(10);
     Node *a = new Node(20);
+    head->next = a;
+
+    cout << head->val << endl;
+    cout << a->val << endl;
+    cout << head->next->val << endl;
+    cout << (*(*head).next).val << endl;
 
     return 0;
 }

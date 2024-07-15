21497d99c1e80cde33465eb466fa3752f6788650
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-07-16 00:21:15 +0600

    Insert at Any Position Implementation in Doubly Linked List

---
+++ insert_at_any_position_in_doubly_linked_list.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print_normal(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+void insert_at_position(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+
+    Node *tmp = head;
+    for (int i = 1; i < pos; i++)
+    {
+        tmp = tmp->next;
+    }
+
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->next->prev = newNode;
+    newNode->prev = tmp;
+
+    /* newNode->next = tmp->next;
+    newNode->prev = tmp;
+    tmp->next->prev = newNode;
+    tmp->next = newNode; */
+}
+
+int size(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *tail = c;
+
+    // Connection
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
+
+    int pos, val;
+    cin >> pos >> val;
+
+    if (pos >= size(head))
+        cout << "Invalid Index" << endl;
+    else
+        insert_at_position(head, pos, val);
+
+    print_normal(head);
+    print_reverse(tail);
+
+    return 0;
+}

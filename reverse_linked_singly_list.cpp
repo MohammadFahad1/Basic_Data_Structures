9cc7070a606e421fc3365148be169324b9203240
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-07-17 18:10:37 +0600

    Reverse Doubly Linked List

--- reverse_linked_singly_list.cpp
+++ reverse_linked_singly_list.cpp
@@ -50,8 +50,12 @@ int main()
     Node *head = new Node(10);
     Node *a = new Node(20);
     Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
     head->next = a;
     a->next = b;
+    b->next = c;
+    c->next = d;
 
     // print_reverse(head);
     reverse(head, head);

ec32e6a0d7e8c4490a20c4492d26a405a54006ee
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-08-30 18:18:58 +0600

    Multiple Input In max Heap

--- input_in_max_heap.cpp
+++ input_in_max_heap.cpp
@@ -3,20 +3,26 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {50, 40, 45, 30, 35, 42, 32, 25, 20, 10};
-    int x;
-    cin >> x;
-    v.push_back(x);
-    int current_idx = v.size() - 1;
+    vector<int> v;
+    int n;
+    cin >> n;
 
-    while (current_idx != 0)
+    while (n--)
     {
-        int parent_idx = (current_idx - 1) / 2;
-        if (v[parent_idx] < v[current_idx])
-            swap(v[parent_idx], v[current_idx]);
-        else
-            break;
-        current_idx = parent_idx;
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int current_idx = v.size() - 1;
+
+        while (current_idx != 0)
+        {
+            int parent_idx = (current_idx - 1) / 2;
+            if (v[parent_idx] < v[current_idx])
+                swap(v[parent_idx], v[current_idx]);
+            else
+                break;
+            current_idx = parent_idx;
+        }
     }
 
     for (int val : v)

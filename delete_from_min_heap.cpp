ea256f24a81990b487c852e976d88f2a8bd3cd8b
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-09-01 19:23:13 +0600

    Delete from min heap isnt complete yet!

---
+++ delete_from_min_heap.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void delete_heap(vector<int> &v)
+{
+    v[0] = v[v.size() - 1];
+    v.pop_back();
+    int cur = 0;
+    while (true)
+    {
+        int left_idx = cur * 2 + 1;
+        int right_idx = cur * 2 + 2;
+        int last_idx = v.size() - 1;
+        if (left_idx <= last_idx && right_idx <= last_idx)
+        {
+            // duitai ache
+            if (v[left_idx] <= v[right_idx] && v[left_idx] < v[cur])
+            {
+                swap(v[left_idx], v[cur]);
+                cur = left_idx;
+            }
+            else if (v[right_idx] <= v[left_idx] && v[right_idx] < v[cur])
+            {
+                swap(v[right_idx], v[cur]);
+                cur = right_idx;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else if (left_idx <= last_idx)
+        {
+            // left ache
+            if (v[left_idx] < v[cur])
+            {
+                swap(v[left_idx], v[cur]);
+                cur = left_idx;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else if (right_idx <= last_idx)
+        {
+            // right ache
+            if (v[right_idx] < v[cur])
+            {
+                swap(v[right_idx], v[cur]);
+                cur = right_idx;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+void print_heap(vector<int> v)
+{
+    for (int val : v)
+        cout << val << " ";
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int current_idx = v.size() - 1;
+
+        while (current_idx != 0)
+        {
+            int parent_idx = (current_idx - 1) / 2;
+            if (v[parent_idx] < v[current_idx])
+                swap(v[parent_idx], v[current_idx]);
+            else
+                break;
+            current_idx = parent_idx;
+        }
+    }
+    delete_heap(v);
+    print_heap(v);
+    delete_heap(v);
+    print_heap(v);
+
+    return 0;
+}

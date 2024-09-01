57f1bf8a66b56f56fb914398bebf04c184572267
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-09-01 19:54:01 +0600

    Count Me Problem Solved

--- Final Exam Problems/Count_Me.cpp
+++ Final Exam Problems/Count_Me.cpp
@@ -5,32 +5,31 @@ int main()
 {
     int t;
     cin >> t;
+    cin.ignore();
+
     while (t--)
     {
-        cin.ignore();
-        string sentence;
-        getline(cin, sentence);
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
         string word;
-        stringstream ss(sentence);
+
         map<string, int> mp;
+
+        string maxName = "";
+        int maxCount = 0;
 
         while (ss >> word)
         {
             mp[word]++;
-        }
-
-        string maxWord;
-        int maxCount = 0;
-        for (auto it = mp.begin(); it != mp.end(); it++)
-        {
-            if (it->second > maxCount)
+            if (mp[word] > maxCount)
             {
-                maxWord = it->first;
-                maxCount = it->second;
+                maxCount = mp[word];
+                maxName = word;
             }
         }
 
-        cout << maxWord << " " << maxCount << endl;
+        cout << maxName << " " << maxCount << endl;
     }
 
     return 0;
